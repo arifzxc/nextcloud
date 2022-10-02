f95dcd3326484e94a363c330a5b582d31509947f
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-03 04:06:26 +0700

    Update linuxubuntu.sh

--- bashshell/linuxubuntu.sh
+++ bashshell/linuxubuntu.sh
@@ -1,14 +1,14 @@
 # Masuk sebagai superuser (root) terlebih dahulu, agar mempermudah proses instalasi.                           
 
+wget https://download.nextcloud.com/server/releases/latest.zip
+
 apt install apache2 php php-xmlrpc php-mysql php-gd php-cli php-curl \
 mysql-client mysql-server libphp-adodb libgd-dev php-pear \
 php-common libapache2-mod-php php-fpm \
 php-bz2 php-xml imagemagick php-zip php-mbstring  -y &
 
-wget https://download.nextcloud.com/server/releases/latest.zip &
-
 cp latest.zip /var/www/html/
-
+cd /var/www/html/
 unzip latest.zip
 
 chmod -R 755 nextcloud/
