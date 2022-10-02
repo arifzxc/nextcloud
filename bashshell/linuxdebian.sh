f650cdec5bc56181ff2cd2286a0f87e3180a56f2
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-03 04:01:26 +0700

    Update linuxdebian.sh

--- bashshell/linuxdebian.sh
+++ bashshell/linuxdebian.sh
@@ -1,13 +1,15 @@
 # Masuk sebagai superuser (root) terlebih dahulu, agar mempermudah proses instalasi. 
 
+wget https://download.nextcloud.com/server/releases/latest.zip &
+
 apt install apache2 php php-xmlrpc php-mysql php-gd php-cli php-curl \
 mariadb-client mariadb-server libphp-adodb libgd-dev php-pear \
 php-common libapache2-mod-php php-fpm \
-php-bz2 php-xml imagemagick php-zip php-mbstring -y &
-
-wget https://download.nextcloud.com/server/releases/latest.zip &
+php-bz2 php-xml imagemagick php-zip php-mbstring -y 
 
 cp latest.zip /var/www/html/
+
+cd /var/www/html/
 
 unzip latest.zip
 
